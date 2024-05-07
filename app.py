95214b818b2f3882601c87e296459b8d34eaaa6f
cierratan <102942911+cierratan@users.noreply.github.com>
2024-05-07 16:05:09 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,jsonify,request
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "Message": "app up and running successfully"
+    })
+
+@app.route("/access",methods=["POST"])
+def access():
+    data = request.get_json()
+    name = data.get("name", "dipto")
+    server = data.get("server","server1")
+
+    message = f"User {name} received access to server {server}"
+
+    return jsonify({
+        "Message": message
+    })
+
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
